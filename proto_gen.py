88e25757c47997292fd62b5a7e17a570c9f7a4c7
Mark Harvey <markedwinharvey@gmail.com>
2016-05-13 15:33:50 -0700

    update html,jquery,gen,readme

--- proto_gen.py
+++ proto_gen.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python
 '''
 web app prototype generator
+prompts for app name; copies and renames all files from proto folder
 '''
 import subprocess,os,time
+
 def main():
+	#----------start program; get filename------------#
 	filename = raw_input('Enter prototype name: ')					#get filename
 	print '### New prototype:',filename,'###'
 	resp = ''
@@ -11,6 +14,8 @@ def main():
 		resp = raw_input('Create this prototype? (y n q): ')		#verify creation
 	if resp in list('qn'):
 		print 'Exiting...';return
+	
+	#-----------create directory/files/subfolders---------#
 	subprocess.Popen(['mkdir '+filename],shell=True)				#create directory
 	home_dir = os.path.expanduser('~')
 	path = home_dir+'/p/proto/proto/'	
@@ -23,15 +28,19 @@ def main():
 				p_file = filename+p_file[5:]
 			with open(filename+'/'+p_file,'w') as f:
 				f.write(doc)
-		else:														#generate static directory
+		#--------------generate static directory--------------#
+		else:														
 			subprocess.Popen(['mkdir '+filename+'/static'],shell=True)
 			static_list = subprocess.Popen(['ls '+path+p_file],stdout=subprocess.PIPE,shell=True).communicate()[0].split()
 			for static_file in static_list:
 				with open(path+p_file+'/'+static_file,'r') as f:
-					doc = f.read().replace('proto',filename)		#substitute new name
-				static_file = filename+static_file[5:]
+					doc = f.read()
+				if static_file[:6] != 'jquery':						#do not alter static jquery min file
+					doc=doc.replace('proto',filename)				#substitute new name	
+				static_file = static_file.replace('proto',filename)
 				with open(filename+'/'+p_file+'/'+static_file,'w') as f:
 					f.write(doc)
+					
 	time.sleep(.5)
 	subprocess.Popen(['chmod +x '+filename+'/*'],shell=True)		#set permissions
 	subprocess.Popen(['chmod +x '+filename+'/static/*'],shell=True)
--- readme.md
+++ readme.md
@@ -1,17 +1,26 @@
+<h4>proto</h4>
+<h5>a web app prototype generator</h5>
 `proto` is a web app prototype generator for creating a basic html/css/javascript/jquery/python
 framework for web apps, packaged with a python server set to run on localhost. 
+
+The generator can be run from any directory by placing a simple script in the .bashrc folder: 
+	proto(){
+		python file-path-to-proto-folder/proto_gen.py
+	}
+
+The command
+	proto
+then prompts for a filename, creates a directory of this name, and places all appropriately-renamed
+files and subfolders within it. A minified copy of jquery is included for off-line development. 
 
 The program can be invoked from any directory by using a bash script
 such as `proto(){python file-path-to-proto-folder/proto_gen.py}`. 
 
-The command to run the `proto` program is then simply `proto`. 
+The app is invoked from the command line with
+	python start.py
+from inside the app's folder. 
 
-The script `proto_gen.py` requests a new file name and copies the contents of the proto folder
-into a newly created folder that has this new name; all files in the new folder are renamed 
-according to the user-specified name. 
+This starts a localhost server on port 8000 (if not already running) and opens the html page. 
 
-The app is launched with the command `python start.py`, which starts the python server and loads 
-the html page in the default browser. 
-
-The 'path' on line 16 of `proto_gen.py` will need to be modified to the directory in which the 
+The 'path' on line 21 of `proto_gen.py` will need to be modified to the directory in which the `proto`
 folder is placed. 
