ee5309067449f7c48c2dc64d0a8de642675f950e
Mark Harvey <markedwinharvey@gmail.com>
2017-01-26 13:45:40 -0800

    add kill_server to start

--- proto/start.py
+++ proto/start.py
@@ -1,10 +1,24 @@
 #!/usr/bin/env python
 import subprocess as sp
 import time
+import requests
+import kill_server
 
+def check_localhost():
+	'''404 message implies a server at port 8000 is running with a root directory elsewhere'''
+	'''if so, kill it'''
+	r = requests.get('http://127.0.0.1:8000/proto')
+	if r.status_code == 404:
+		print 'Got 404 message; attempting server kill'
+		kill_server.main()
+	else:
+		print 'Returned status code:',r.status_code
+		
+	start()
 
-def main():
+def start():
 	try:
+		print 'Starting new python server on port 8000'
 		cmd = 'python server.py &'.split()
 		sp.Popen(cmd)
 		time.sleep(.5)	
@@ -16,6 +30,16 @@ def main():
 		time.sleep(.5)
 		print 'Cannot start server.'
 		
+		
+def main():
+	try:
+		check_localhost()
+		
+	except:
+		'''no connection to localhost found; start server'''
+		start()
+
+	
 		
 if __name__ == '__main__':
 	main()
