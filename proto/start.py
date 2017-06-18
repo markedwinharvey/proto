11bf207085812dfabd55a725bfa4b45d07a827d2
Mark Harvey <markedwinharvey@gmail.com>
2017-06-18 07:24:50 +0200

    handle bad port input

--- proto/start.py
+++ proto/start.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python
 '''
 Script to start localhost server (as server.py)
-1. Get port number for new server
+1. Get port number for new server (default is 8000); confirm port is between 1-65535
 2. Check for running server and kill if necessary
 3. Rewrite new port numbers to html file if necessary
+
+Usage:
+	$ python start.py [port]
 '''
 
 import subprocess as sp
@@ -58,7 +61,9 @@ def start( port ):
 #	
 #		
 def write_new_port( port ): 
-	''' Rewrite proto.html to fill in appropriate port number (port number will be rewritten even if unchanged) '''
+	''' Rewrite proto.html to fill in appropriate port number (port number will be rewritten even if unchanged) 
+		This will overwrite port numbers for all local urls within {{proto}}.html (i.e., script/link tags)
+	'''
 	
 	with open('proto.html','r') as f:
 		doc = f.read()
@@ -72,12 +77,36 @@ def write_new_port( port ):
 		f.write(doc)
 
 #
+#
+def get_port():
+	'''	Retrieve port via sys.argv, raw_input, or set default (8000)
+		Ensure port is valid; otherwise raise excetion
+	'''
+	if len(sys.argv) > 1:
+		port = sys.argv[1]
+	else:
+		port = raw_input('Enter port number or hit enter for default (8000): ')
+	if not port:
+		port = 8000
+	
+	try:
+		port = int(port)
+		if not 1024 <= int(port) <= 65535:
+			raise Exception
+	except Exception as e:
+		print 'Port must be a number 1024-65535'
+		print 'Traceback:'
+		print e
+		sys.exit()
+		
+	return port
+#
 #		
 def main():
 	''' Get port number or accept 8000 as default; check for localhost connection; kill if necessary, then start new server'''
 	
-	port = raw_input('Enter port number or hit enter for default (8000): ')
-	if not port: port = 8000
+	port = get_port()
+	
 	print 'Initializing server'
 	
 	try:
